d43ffbfb02045e560c224462df49a15c6759f036
Saurav Sahu <sauravsahu.12@live.com>
2018-02-09 21:32:40 +0530

    Add program to demonstrate use of command line arguments.
    Add program to demonstrate use of command line arguments.

---
+++ CommandLineArgs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace CommandLineArgs
+{
+  class Program
+  {
+    public static void Main(string []arguments)
+    {
+      arguments.Reverse().ToList().ForEach(argument => {
+        Console.WriteLine(argument);
+      });
+    }
+  }
+}
