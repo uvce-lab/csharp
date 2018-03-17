e5ffe89ae1e2668f8a25b822c961f1609329be07
Saurav Sahu <sauravsahu.12@live.com>
2018-03-17 20:02:24 +0530

    Add basic stack implementation

---
+++ Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharpLab.Stack
+{
+  class Program
+  {
+    static void Main()
+    {
+      var stack = new Stack<string>();
+
+      do
+      {
+        Console.Write("STACK MENU\n1. Push Item\n2. Pop Item\nEnter your choice: ");
+
+        var choice = Console.ReadLine().Trim();
+
+        switch (choice)
+        {
+          case "1":
+            Console.Write("Enter item to push: ");
+            var item = Console.ReadLine().Trim();
+            stack.Push(item);
+            break;
+
+          case "2":
+            try
+            {
+              var poppedItem = stack.Pop();
+              Console.WriteLine($"Item popped: {poppedItem}");
+            }
+            catch (Exception e)
+            {
+              Console.WriteLine(e.Message);
+            }
+            break;
+
+          default:
+            Console.WriteLine("You entered an invalid choice");
+            break;
+        }
+      } while (true);
+    }
+  }
+}
