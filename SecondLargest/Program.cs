5724067ebbf3c1e3f6f779d4ae9a3df953421ab0
Saurav Sahu <sauravsahu.12@live.com>
2018-03-17 21:05:04 +0530

    Calculate second largest number with LINQ

---
+++ SecondLargest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace SecondLargest
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.Write("Enter the numbers: ");
+
+      try
+      {
+        var numbers = Console.ReadLine().Trim();
+
+        var secondLargest = numbers
+        .Split()
+        .Select(double.Parse)
+        .OrderByDescending(p => p)
+        .Take(2)
+        .LastOrDefault();
+
+        Console.WriteLine($"The second largest number: {secondLargest}");
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
+  }
+}
