4ff61f0f6949ed83383bec911166142f328af1a2
Saurav Sahu <sauravsahu.12@live.com>
2018-03-18 19:45:31 +0530

    Add program to calculate sum of items in a jagged array.

---
+++ JaggedArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace JaggedArray
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Enter the number of rows: ");
+      var rows = int.Parse(Console.ReadLine());
+
+      var jaggedArray = new double[rows][];
+
+      for (int i = 0; i < rows; ++i)
+      {
+        Console.WriteLine($"Enter elements for row {i}:");
+        jaggedArray[i] = Console.ReadLine()
+          .Trim()
+          .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+          .Select(double.Parse)
+          .ToArray();
+      }
+
+      Console.WriteLine("Jagged Array:");
+      jaggedArray.ToList().ForEach(p => Console.WriteLine(String.Join(" ", p)));
+
+      var sum = (from row in jaggedArray
+                 from item in row
+                 select item).Sum();
+
+      Console.WriteLine($"Sum of elements in the jagged array: {sum}");
+    }
+  }
+}
