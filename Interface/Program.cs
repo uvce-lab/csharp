2ded33e65a42ea8aa65140216424e6dba24d4a1e
Saurav Sahu <sauravsahu.12@live.com>
2018-03-21 19:29:09 +0530

    Add Student class and implement IComparable to sort it's instances.
    Add Student class and implement IComparable to sort it's instances.

---
+++ Interface/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpLab.Interface
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var students = new List<Student>
+      {
+        new Student("Harrison Wells", "14GAEC9004"),
+        new Student("Aaron S","14GAEC9001"),
+        new Student("Diana P","14GAEC9003"),
+        new Student("Barry Allen","14GAEC9002"),
+      };
+
+      students.Sort();
+
+      Console.WriteLine("Sorted students: ");
+      students.ForEach(Console.WriteLine);
+    }
+  }
+}
