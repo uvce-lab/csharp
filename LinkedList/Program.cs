bc43bb284467a9a4a0934a908ea1bb9896135484
Saurav Sahu <sauravsahu.12@live.com>
2018-03-21 12:11:59 +0530

    Add main program with insertion and the beginning and display functions.
    Add main program with insertion and the beginning and display functions.

---
+++ LinkedList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpLab.LinkedList
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var list = new LinkedList<string>();
+
+      do
+      {
+        Console.Write(@"
+MENU:
+1. Add item to the beginning
+2. Display the list
+3. Exit
+Enter your choice: ");
+
+        var choice = Console.ReadLine().Trim();
+
+        switch (choice)
+        {
+          case "1":
+            Console.Write("Enter item to insert at the beginning: ");
+            var item = Console.ReadLine().Trim();
+            list.AddFirst(item);
+            break;
+
+          case "2":
+            Console.WriteLine($"The list contains:\n[{String.Join(" --> ", list)}]");
+            break;
+          
+          case "3":
+            return;
+          
+          default:
+            Console.WriteLine("You entered an invalid choice: ");
+            break;
+        }
+      } while (true);
+    }
+  }
+}
