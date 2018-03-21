53bef81cad4c473fc0e16eae01b03abb6928b5de
Saurav Sahu <sauravsahu.12@live.com>
2018-03-21 21:01:58 +0530

    Add IShape interface and project
    Add IShape interface and project

---
+++ InterfaceArrays/IShape.cs
@@ -0,0 +1,9 @@
+namespace CSharpLab.InterfaceArrays
+{
+  public interface IShape
+  {
+    void Input();
+    double Area();
+    double NumberOfPoints();
+  }
+}
---
+++ InterfaceArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
